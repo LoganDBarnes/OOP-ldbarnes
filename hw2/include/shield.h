b1a277a3cffc24d46f7710483f220991aa605ec7
Logan Barnes <ldbarnes@hotmail.com>
2021-02-09 17:05:59 -0700

    Create cpp object

---
+++ hw2/include/shield.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+namespace shield {
+    
+    class Shield {
+        private: 
+        std::string m_style;
+        std::string m_material;
+        int m_weight;
+        std::string m_color;
+
+        public: Shield(std::string style, std::string material, int weight, std::string color, bool thrown);
+
+        public: void throwShield();
+        public: bool thrown();
+        
+    };
+}
---
+++ hw2/src/shield.cpp
@@ -0,0 +1,7 @@
+#include "shield.h"
+#include <stdexcept>
+
+namespace shield{
+
+    Shield::Shield(std::string style, std::string material, int weight, std::string color, bool thrown)
+}
