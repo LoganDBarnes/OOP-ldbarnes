2bb8e76840f72c763023bba9ae651950c19597d0
Logan Barnes <ldbarnes@hotmail.com>
2021-02-23 11:41:25 -0700

    Add new test

--- hw2/src/shield_test.cpp
+++ hw2/src/shield_test.cpp
@@ -19,6 +19,27 @@ TEST(Shield, Constructor) {
   ASSERT_EQ(shield.thrown(), thrown);
 }
 
+TEST(Shield, shieldThrow) {
+  string style = "round";
+  string material = "vibranium";
+  string color = "red, white, and blue";
+  int weight = 12;
+  bool thrown = false;
+
+  Shield shield(style, material, color, weight, thrown);
+  ASSERT_EQ(shield.style(), style);
+  ASSERT_EQ(shield.material(), material);
+  ASSERT_EQ(shield.color(), color);
+  ASSERT_EQ(shield.weight(), weight);
+  ASSERT_EQ(shield.thrown(), thrown);
+
+  if (shield.thrown == false) {
+    print("Captain America throws his mighty shield!");
+    shield.throw();
+    ASSERT_EQ(shield.thrown(), true);
+  }
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
