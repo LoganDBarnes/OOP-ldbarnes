9755bbe0c709e384b602e879d28222aaef29694d
Logan Barnes <ldbarnes@hotmail.com>
2021-02-23 11:33:02 -0700

    Update tests

--- hw2/src/shield_test.cpp
+++ hw2/src/shield_test.cpp
@@ -2,11 +2,21 @@
 #include "gtest/gtest.h"
 
 using namespace std;
+using namespace shield;
 
-TEST(Hello, HelloWorld) {
-  string expect = "Hello, World!";
-  string result = hello("World");
-  ASSERT_EQ(expect,result);
+TEST(Shield, Constructor) {
+  string style = "round";
+  string material = "steel";
+  string color = "silver";
+  int weight = 10;
+  bool thrown = false;
+
+  Shield shield(style, material, color, weight, thrown);
+  ASSERT_EQ(shield.style(), style);
+  ASSERT_EQ(shield.material(), material);
+  ASSERT_EQ(shield.color(), color);
+  ASSERT_EQ(shield.weight(), weight);
+  ASSERT_EQ(shield.thrown(), thrown);
 }
 
 int main(int argc, char** argv) {
