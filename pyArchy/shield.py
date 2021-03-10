744c6916e64cdcbf2eff5c5f8289a4951208a840
Logan Barnes <ldbarnes@hotmail.com>
2021-03-09 17:39:51 -0700

    Add parent class

--- pyArchy/shield.py
+++ pyArchy/shield.py
@@ -1,10 +1,22 @@
-class Shield:
+class Armor:
+    def __init__(self, material : str,  defense: int) :
+        self._material = material
+        self._defense = defense
 
-    def __init__(self, style : str, material : str, weight : int, color: str, thrown: bool) -> None:
+    @property
+    def material(self) -> str:
+        return self._material
+
+    @property
+    def defense(self):
+        return self._defense
+
+
+class Shield:
+    def __init__(self, style : str, color : int, weight: str, thrown: bool) -> None:
         self._style : str = style
-        self._material : str = material
-        self._weight : int = weight
         self._color : str = color
+        self._weight : int = weight
         self._thrown : bool = thrown
 
     @property
@@ -12,16 +24,12 @@ class Shield:
         return self._style
 
     @property
-    def material(self) -> str:
-        return self._material
+    def color(self) -> str:
+        return self._color
 
     @property
     def weight(self):
         return self._weight
-
-    @property
-    def color(self) -> str:
-        return self._color
 
     @property
     def thrown(self) -> str:
--- pyArchy/shieldtest.py
+++ pyArchy/shieldtest.py
@@ -5,30 +5,26 @@ from shield import Shield
 class ShieldTest(unittest.TestCase):
     def testDefaultShield(self):
         style : str = "round"
-        material : str = "steel"
-        weight : int = 10
         color : str = "silver"
+        weight : int = 10
         thrown : bool = False
 
-        shield : Shield = Shield(style, material, weight, color, thrown)
+        shield : Shield = Shield(style, color, weight, thrown)
         self.assertEqual(shield.style,style)
-        self.assertEqual(shield.material,material)
-        self.assertEqual(shield.weight,weight)
         self.assertEqual(shield.color,color)
+        self.assertEqual(shield.weight,weight)
         self.assertEqual(shield.thrown,thrown)
 
     def testThrow(self):
         style : str = "round"
-        material : str = "vibranium"
-        weight : int = 12
         color : str = "red, white, and blue"
+        weight : int = 12
         thrown : bool = False
 
-        shield : Shield = Shield(style, material, weight, color, thrown)
+        shield : Shield = Shield(style, color, weight, thrown)
         self.assertEqual(shield.style,style)
-        self.assertEqual(shield.material,material)
-        self.assertEqual(shield.weight,weight)
         self.assertEqual(shield.color,color)
+        self.assertEqual(shield.weight,weight)
         self.assertEqual(shield.thrown,thrown)
 
         if shield.thrown == False:
