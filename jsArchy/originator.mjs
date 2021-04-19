6b2676f4f79e437aa6e3e40d8bc8fe271b97dbec
Logan Barnes <ldbarnes@hotmail.com>
2021-04-19 13:27:35 -0600

    Translate originator class

--- jsArchy/originator.mjs
+++ jsArchy/originator.mjs
@@ -1,13 +1,26 @@
+import { Memento } from "./memento.mjs";
+
 export class Originator {
     constructor(string1) {
         this._string1 = string1;
     }
 
-    get string1() {
-        return this._string1;
+    set string1() {
+        this._string1 = string1;
     }
 
-    
+    saveToMemento() {
+        Memento;
+        return Memento;
+    }
+
+    restore(memento1){ //from memento
+        m_string1 = memento1.string1();
+    }
+
+    get value(){
+        return m_string;
+    }
 }
 
 module.exports = {'Originator': Originator}
